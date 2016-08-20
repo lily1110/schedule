0a1819c67f27234ac6a560115d484fdc8935ea4c
lilili <1570115630@qq.com@qq.com>
2016-08-20 18:55:17 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+编译方式：
+1、在命令行，进入src/main/webapps/schedule
+2、在命令行，输入 ./make.sh prod 可以编译生产用代码
+3、在命令行，输入 ./make.sh dev 可以编译开发用代码
--- src/main/webapps/schedule/make.sh
+++ src/main/webapps/schedule/make.sh
@@ -1,11 +1,9 @@
 echo $1
 if [ $1 = 'dev' ]; then
     cp schedule.html.dev schedule.html
-    cp js/schedule/TypeStore.js.dev js/schedule/TypeStore.js
     npm start
 fi
 if [ $1 = 'prod' ]; then
     cp schedule.html.prod schedule.html
-    cp js/schedule/TypeStore.js.prod js/schedule/TypeStore.js
     npm run build
 fi
